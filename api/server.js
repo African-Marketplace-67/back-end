1154cbabef9d7cdd2f25acf6c320f984ad854c78
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2021-02-02 11:20:06 -0800

    restricted paths to router

--- api/server.js
+++ api/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 const authRouter = require('./auth/auth-router.js');
 const itemRouter = require('./items/item-router')
-
+const restricted = require('./middleware/restricted')
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
-server.use('/api/items', itemRouter)
+server.use('/api/items',restricted, itemRouter)
 //server.use('/api/jokes', restrict, jokesRouter)
 // DECIDE ON AUTHENTICATION AND IMPLEMENT FOR ROUTER. ROLLBACK ON LOCAL AND HEROKU BEFORE CHANGING ANYTHING!!!!
 //this iss and aexttra comment

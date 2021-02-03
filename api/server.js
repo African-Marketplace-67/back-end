27b531afe305cef6e329f62d228a856a5b8d6e79
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2021-02-03 09:03:01 -0800

    cors'

--- api/server.js
+++ api/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors')
 const authRouter = require('./auth/auth-router.js');
 const itemRouter = require('./items/item-router')
 const restricted = require('./middleware/restricted')
+
 const server = express();
 
+server.use(cors())
+server.use(helmet())
 server.use(express.json());
 
 server.use('/auth', authRouter);
 server.use('/items',restricted, itemRouter)
-//server.use('/api/jokes', restrict, jokesRouter)
-// DECIDE ON AUTHENTICATION AND IMPLEMENT FOR ROUTER. ROLLBACK ON LOCAL AND HEROKU BEFORE CHANGING ANYTHING!!!!
+
 //this iss and aexttra comment
 module.exports = server;
